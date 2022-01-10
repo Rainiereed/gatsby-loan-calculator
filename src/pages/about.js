31906f793bbf2eae6eb15f94c94fcb96f1bec72d
Wei Lu <wei.lu@hyperisland.se>
2022-01-10 17:08:44 +0100

    styling

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
